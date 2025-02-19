025d1c2b3728cb82863ba94974c995dbcab1f3c5
juandavid764 <juandaviderazo2401@gmail.com>
2025-02-19 15:06:34 -0500

    first commit challenge 02

---
+++ hello_world/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import FirstApp from  "./components/FirstApp";
+
+function App() {
+  return (
+    <>
+      <h1>Hola mundo!</h1>
+      <h2>Bienvenido</h2>
+      <FirstApp />
+    </>
+  );
+}
+
+export default App;
---
+++ hello_world/src/components/FirstApp.jsx
@@ -0,0 +1,12 @@
+const FirstApp = () => {
+    const title = 'First App';
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>10</span>
+    </>
+  );
+}
+
+export default FirstApp;
