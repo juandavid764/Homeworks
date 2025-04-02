24ea8dc6bf40cf112998f96c2e01e1e9117b4555
juandavid764 <juandaviderazo2401@gmail.com>
2025-04-02 14:41:11 -0500

    challenge_01 finished

---
+++ funciones.js
@@ -0,0 +1,17 @@
+//Es par?
+function isEvenRegular(number) {
+    if (number % 2 === 0) {
+        console.log("Es par");
+    }    else {
+        console.log("Es impar");
+    }
+}
+
+//Es par?
+const isEvenArrow = (number) => { 
+    if (number % 2 === 0) {
+        console.log("Es par");
+    } else {
+        console.log("Es impar");
+    }
+}
--- hello_world/src/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slices/CounterSlice.jsx";
-
-export const Store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
