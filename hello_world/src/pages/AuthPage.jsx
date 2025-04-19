d53b673dce3529fbbb3f7238523c50ed9e0e40b8
juandavid764 <juandaviderazo2401@gmail.com>
2025-04-18 21:20:14 -0500

    details

--- hello_world/src/pages/AuthPage.jsx
+++ hello_world/src/pages/AuthPage.jsx
@@ -11,9 +11,6 @@ export const AuthPage = () => {
       {authProcess == "loginWithEmail" ? (
         <Login changestateAuth={setAuthProcess} />
       ) : null}
-      {authProcess == "loginWithGoogle" ? (
-        <LoginWithGoogle changestateAuth={setAuthProcess} />
-      ) : null}
       {authProcess == "createAccount" ? (
         <Registro changestateAuth={setAuthProcess} />
       ) : null}
